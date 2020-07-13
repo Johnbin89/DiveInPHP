b03adaf197350701228f580afd6adb25edb017b0
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2020-07-13 13:45:40 +0300

    move db credentials to env variables

--- app/dbconfig.php
+++ app/dbconfig.php
@@ -1,9 +1,9 @@
 <?php
-define('DB_SERVER', '***REMOVED***');
-define('DB_USERNAME', '***REMOVED***');
-define('DB_PASSWORD', '***REMOVED***');
-define('DB_NAME', '***REMOVED***');
-define('DB_PORT', '***REMOVED***');
+define('DB_SERVER', getenv('ENV_DB_SERVER'));
+define('DB_USERNAME', getenv('ENV_DB_USERNAME'));
+define('DB_PASSWORD', getenv('ENV_DB_PASSWORD'));
+define('DB_NAME', getenv('ENV_DB_NAME'));
+define('DB_PORT', getenv('ENV_DB_PORT'));
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
